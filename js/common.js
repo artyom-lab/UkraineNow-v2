9ef7f9adb9cef835c5f68bc5d836031251da1075
artyom-lab <artyomgalkeane@gmail.com>
2023-04-11 18:20:10 +0300

    59

--- js/common.js
+++ js/common.js
@@ -54,33 +54,89 @@ function autosize(){
     }
 }
 
-var swiper = new Swiper(".slide-container", {
-  slidesPerView: 3,
-  spaceBetween: 15,
-  // slidesPerGroup: 1,
-  loop: true,
-  centerSlide: "true",
-  speed: 1000,
-  touchRatio: 3,
-  fade: "true", 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    // dynamicBullets: true,
-  },
-  breakpoints: {
-    0: {
-      allowTouchMove: true,
-    },
-    1200: {
-      allowTouchMove: false,
-    },
-  },
-});
+
+// var swiper = new Swiper(".slide-container", {
+//   slidesPerView: 3,
+//   spaceBetween: 15,
+//   // slidesPerGroup: 1,
+//   loop: true,
+//   centerSlide: "true",
+//   speed: 1000,
+//   touchRatio: 1,
+//   fade: "true", 
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     // dynamicBullets: true,
+//   },
+//   breakpoints: {
+//     0: {
+//       allowTouchMove: true,
+//     },
+//     1200: {
+//       allowTouchMove: false,
+//     },
+//   },
+// });
+
+
+(function() {
+  var MenuWebCarousel, carousel;
+
+  MenuWebCarousel = (function() {
+    function MenuWebCarousel() {
+      this.swiperContainer = $("#carousel");
+      this.swiperOptions = {
+        resistanceRatio: 0,
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto'
+      };
+    }
+
+    MenuWebCarousel.prototype.initSwiper = function() {
+      return this.swiperInstance = new Swiper(this.swiperContainer[0], this.swiperOptions);
+    };
+
+    return MenuWebCarousel;
+
+  })();
+
+  carousel = new MenuWebCarousel();
+
+  carousel.initSwiper();
+
+}).call(this);
 
 
 });
 
+// (function() {
+//   var MenuWebCarousel, carousel;
 
+//   MenuWebCarousel = (function() {
+//     function MenuWebCarousel() {
+//       this.swiperContainer = $("#carousel");
+//       this.swiperOptions = {
+//         resistanceRatio: 0,
+//         spaceBetween: 10,
+//         centeredSlides: true,
+//         slidesPerView: 'auto'
+//       };
+//     }
+
+//     MenuWebCarousel.prototype.initSwiper = function() {
+//       return this.swiperInstance = new Swiper(this.swiperContainer[0], this.swiperOptions);
+//     };
+
+//     return MenuWebCarousel;
+
+//   })();
+
+//   carousel = new MenuWebCarousel();
+
+//   carousel.initSwiper();
+
+// }).call(this);
 
 
